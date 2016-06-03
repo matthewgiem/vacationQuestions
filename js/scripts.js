8988708fffd63115c0fe4a3037cbfede21b43e0d
ada lovelace and matthew giem <matthew@>
2016-06-03 11:28:56 -0700

    created the function that finds the highest value while on a branch

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,11 @@ var q3;
 var q4;
 var q5;
 var q6;
+// q15 is sun
 var q15;
+// q23 is outdoors
 var q23;
+// q46 is city
 var q46;
 
 //back end logic (decision maker)
@@ -31,7 +34,35 @@ var fourAndSix = function() {
 alert("hi");
 
 
+// function that finds out what has the higher score sun city outdoors
 
+var compare1 = function()  {
+  if ( q15 == q46 == q23 ) {
+    // they are tied at the top
+    alert('they are tied');
+  } else if ( q15 > q23 && q15 > q46 ) {
+    // then q15 is the highest
+    alert("You're going to the Sun");
+  } else if ( q23 > q15 && q23 > q46 ) {
+    // then q23 is the highest
+    alert("You're going to the Ocean");
+  } else if ( q46 > q15 && q46 > q23 ) {
+    // the q46 is the highest
+    alert("You're going to the city" );
+  } else if ( q15 === q23 && q15 > q46 )  {
+    // we need to choose between q15 and q23
+    alert('you need to choose between the sun and outdoors');
+  } else if ( q15 === q46 && q15 > q23 )  {
+    // we need to choose between q15 and q46
+    alert('you need to choose between the sun and the city');
+  } else if ( q46 === q23 && q46 > q15 )  {
+    // we need to choose between q46 and q23
+    alert('you need to choose between the outdoors and the city')
+  } else {
+    // we need to run the operation again cause we are getting an error
+    alert("something is wrong with your application try and subbmitting it agian if it doesn't work don't worry we allready have your ssn so we don't need anything else")
+  }
+}
 
 
 
@@ -54,6 +85,7 @@ $(function()  {
     fourAndSix();
     alert("hi");
     alert(q15);
+    compare1();
 
   });
 
