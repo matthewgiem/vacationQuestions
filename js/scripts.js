6ede356ad1fa6485ac4c1ad1df91e15e152f5fae
ada lovelace and matthew giem <ada@>
2016-06-03 10:21:16 -0700

    created and tested functions that add up specific answers

--- js/scripts.js
+++ js/scripts.js
@@ -10,21 +10,38 @@ var q3;
 var q4;
 var q5;
 var q6;
+var q15;
+var q23;
+var q46;
 
 //back end logic (decision maker)
+// add up questions 1 & 5 to see how much you like the sun
+var oneAndFive = function() {
+  q15 = q1 + q5;
+}
+
+var twoAndThree = function() {
+  q23 = q2 + q3;
+}
+
+var fourAndSix = function() {
+  q46 = q4 + q6;
+}
 
 
 
 
 
-//front end logic (interacts with webpage)
+
+
+
+// front end logic (interacts with the website)
 $(function()  {
-  $(form.blank).submit(function(event)  {
-    name = $(#"name").val();
-    dob = $(#"born").val();
-    sex = $(#"pref").val();
-    ssn = $(#"").val();
-    realPref = $(#"realPref").val();
+  $("form.blank").submit(function(event) {
+    name = $("#name").val();
+    dob = $("#born").val();
+    sex = $("#pref").val();
+    realPref = $("#realPref").val();
     q1 = parseInt($("input:radio[name=q1]:checked").val());
     q2 = parseInt($("input:radio[name=q2]:checked").val());
     q3 = parseInt($("input:radio[name=q3]:checked").val());
@@ -32,5 +49,9 @@ $(function()  {
     q5 = parseInt($("input:radio[name=q5]:checked").val());
     q6 = parseInt($("input:radio[name=q6]:checked").val());
     event.preventDefault();
-  })
-})
+    oneAndFive();
+    twoAndThree();
+    fourAndSix();
+  });
+
+});
